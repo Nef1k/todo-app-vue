21e22f842468a6d811b112187b83150150499f88
Valeriy Nefedov <nef1k@outlook.com>
2020-08-28 03:27:05 +0300

    Remove commented code

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,4 @@
 <template>
-<!--  <div class="about">-->
-<!--    <h1>About</h1>-->
-<!--    <p>-->
-<!--      This is the ToDo List app v1.0.0. This is a part of the Vue crash course on YouTube.-->
-<!--    </p>-->
-<!--  </div>-->
   <v-row
     align="center"
     justify="center"
@@ -24,10 +18,3 @@
     </v-col>
   </v-row>
 </template>
-
-<style scoped>
-  .shrinker {
-    width: 100px;
-    text-align: center;
-  }
-</style>
